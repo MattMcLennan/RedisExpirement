6d824a38816bc9e0b3f7fd7517416edcd211985c
MattMcLennan <mclennan6891@gmail.com>
2020-03-09 23:20:39 -0400

    Added performance for redis

--- Program.cs
+++ Program.cs
@@ -1,8 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using ServiceStack.Redis;
-using ServiceStack.Text;
 
 namespace RedisExpirement {
     class Program {
@@ -12,19 +11,85 @@ namespace RedisExpirement {
                 // Adding full list to redis
                 // Get all indexes as a time table
                 // want to see how time changes for different items in the list
-                // https://stackoverflow.com/questions/969290/exact-time-measurement-for-performance-testing
 
-                var mockedData = new MockedData (300000);
-                List<string> result = mockedData.Records.Select (x => x.ToString ()).ToList ();
-                redis.AddRangeToList ("1", result);
+                redis.FlushDb ();
+                var data = new MockedData (300000);
 
-                var listCount = redis.GetListCount ("1");
-                Console.WriteLine (listCount);
+                var list = new RedisList (Guid.NewGuid ().ToString (), redis, data);
+                var set = new RedisSortedSet (Guid.NewGuid ().ToString (), redis, data);
+
+                Console.WriteLine ("List Testing");
+                PerformanceTesting.TimeOperation ("Insert all data into a list", list.AddAllRecords);
+                PerformanceTesting.TimeOperation ("Lookup all records sequentially - one by one", list.LookupRecordsSequentially);
+
+                Console.WriteLine ("++++++++++============================================++++++++++");
+
+                Console.WriteLine ("Sorted Set Testing");
+                PerformanceTesting.TimeOperation ("Insert all data into a sortedset", set.AddAllRecords);
+                PerformanceTesting.TimeOperation ("Lookup all records sequentially - one by one", set.LookupRecordsSequentially);
             }
         }
 
-        public static class Redis {
+        public static class PerformanceTesting {
+            public static TimeSpan TimeOperation (string message, Action action) {
+                var sw = Stopwatch.StartNew ();
+                action ();
+                sw.Stop ();
 
+                Console.WriteLine ($"Testing {message} - Elapsed = {sw.Elapsed}");
+                return sw.Elapsed;
+            }
+        }
+
+        public interface IRedis {
+            void AddAllRecords (MockedData data);
+            void LookupRecord (int n);
+            void LookupRecordsSequentially ();
+        }
+
+        public abstract class Redis {
+            protected string Identifier { get; set; }
+            protected long[] Records { get; set; }
+            protected RedisClient RedisClient { get; set; }
+            public Redis (string identifier, RedisClient redisClient, MockedData data) {
+                Identifier = identifier;
+                Records = data.Records;
+                RedisClient = redisClient;
+            }
+
+            public abstract void AddAllRecords ();
+            public abstract void LookupRecord (int index);
+
+            public void LookupRecordsSequentially () {
+                for (int i = 0; i < Records.Length; i++)
+                    LookupRecord (i);
+            }
+        }
+
+        public class RedisList : Redis {
+            public RedisList (string identifier, RedisClient redisClient, MockedData data) : base (identifier, redisClient, data) { }
+
+            public override void AddAllRecords () {
+                var result = Records.Select (x => x.ToString ()).ToList ();
+                RedisClient.AddRangeToList ("1", result);
+            }
+
+            public override void LookupRecord (int index) {
+                var record = RedisClient.GetItemFromList (Identifier, index);
+            }
+        }
+
+        public class RedisSortedSet : Redis {
+            public RedisSortedSet (string identifier, RedisClient redisClient, MockedData data) : base (identifier, redisClient, data) { }
+
+            public override void AddAllRecords () {
+                for (var i = 0; i < Records.Length; i++)
+                    RedisClient.AddItemToSortedSet (Identifier, Records[i].ToString (), i);
+            }
+
+            public override void LookupRecord (int index) {
+                var record = RedisClient.GetRangeFromSortedSet (Identifier, index, index);
+            }
         }
 
         public class MockedData {
